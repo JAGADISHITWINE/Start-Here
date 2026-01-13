285b0540641a76a710a3864da59b1a2be922593e
Jagadish S <jagadishitwinetech@gmail.com>
2026-01-13 17:52:47 +0530

    update

--- start-here/src/app/faqs/faqs.component.ts
+++ start-here/src/app/faqs/faqs.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 interface FAQ {
@@ -15,7 +16,7 @@ interface FAQ {
   templateUrl: './faqs.component.html',
   styleUrls: ['./faqs.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
 })
 export class FaqsComponent implements OnInit {
 
